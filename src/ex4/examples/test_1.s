59ec9a0213076f77e2289d8b9aae51f312bfe080
David Trabish <davidtr1037@gmail.com>
2021-12-20 19:37:51 +0200

    updating

--- src/ex4/examples/test_1.s
+++ src/ex4/examples/test_1.s
@@ -3,33 +3,86 @@
 .text
 
 f:
+# prologue
 subu $sp, $sp, 4
 sw $ra 0($sp)
 subu $sp, $sp, 4
 sw $fp 0($sp)
 move $fp, $sp
+subu $sp, $sp, 4
+sw $t0, 0($sp)
+subu $sp, $sp, 4
+sw $t1, 0($sp)
+subu $sp, $sp, 4
+sw $t2, 0($sp)
+subu $sp, $sp, 4
+sw $t3, 0($sp)
+subu $sp, $sp, 4
+sw $t4, 0($sp)
+subu $sp, $sp, 4
+sw $t5, 0($sp)
+subu $sp, $sp, 4
+sw $t6, 0($sp)
+subu $sp, $sp, 4
+sw $t7, 0($sp)
+subu $sp, $sp, 4
+sw $t8, 0($sp)
+subu $sp, $sp, 4
+sw $t9, 0($sp)
 sub $sp, $sp, 16
 
 lw $t0, 8($fp)
 lw $t1, 12($fp)
 add $t2, $t0, $t1
-sw $t2, -4($fp)
-lw $v0, -4($fp)
+sw $t2, -44($fp)
+lw $t3, -44($fp)
+move $v0, $t3
 j f_epilogue
 
 f_epilogue:
 move $sp, $fp
+lw $t0, -4($sp)
+lw $t1, -8($sp)
+lw $t2, -12($sp)
+lw $t3, -16($sp)
+lw $t4, -20($sp)
+lw $t5, -24($sp)
+lw $t6, -28($sp)
+lw $t7, -32($sp)
+lw $t8, -36($sp)
+lw $t9, -40($sp)
 lw $fp, 0($sp)
 lw $ra, 4($sp)
 addu $sp, $sp, 4
 jr $ra
 
 user_main:
+# prologue
 subu $sp, $sp, 4
 sw $ra 0($sp)
 subu $sp, $sp, 4
 sw $fp 0($sp)
 move $fp, $sp
+subu $sp, $sp, 4
+sw $t0, 0($sp)
+subu $sp, $sp, 4
+sw $t1, 0($sp)
+subu $sp, $sp, 4
+sw $t2, 0($sp)
+subu $sp, $sp, 4
+sw $t3, 0($sp)
+subu $sp, $sp, 4
+sw $t4, 0($sp)
+subu $sp, $sp, 4
+sw $t5, 0($sp)
+subu $sp, $sp, 4
+sw $t6, 0($sp)
+subu $sp, $sp, 4
+sw $t7, 0($sp)
+subu $sp, $sp, 4
+sw $t8, 0($sp)
+subu $sp, $sp, 4
+sw $t9, 0($sp)
 sub $sp, $sp, 16
 
 li $t0, 2
@@ -41,9 +94,9 @@ sw $t1 0($sp)
 jal f
 addu $sp, $sp, 8
 move $t2, $v0
-sw $t2, -4($fp)
+sw $t2, -44($fp)
 # inline implementation of PrintInt
-lw $t3, -4($fp)
+lw $t3, -44($fp)
 li $v0, 1
 move $a0, $t3
 syscall
@@ -51,6 +104,16 @@ j user_main_epilogue
 
 user_main_epilogue:
 move $sp, $fp
+lw $t0, -4($sp)
+lw $t1, -8($sp)
+lw $t2, -12($sp)
+lw $t3, -16($sp)
+lw $t4, -20($sp)
+lw $t5, -24($sp)
+lw $t6, -28($sp)
+lw $t7, -32($sp)
+lw $t8, -36($sp)
+lw $t9, -40($sp)
 lw $fp, 0($sp)
 lw $ra, 4($sp)
 addu $sp, $sp, 4
