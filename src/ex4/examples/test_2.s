b83bcb75ba794b7b8e5404cda3251c1f277c5fc8
David Trabish <davidtr1037@gmail.com>
2021-12-20 19:40:21 +0200

    updating

--- src/ex4/examples/test_2.s
+++ src/ex4/examples/test_2.s
@@ -10,6 +10,26 @@ sw $ra 0($sp)
 subu $sp, $sp, 4
 sw $fp 0($sp)
 move $fp, $sp
+subu $sp, $sp, 4
+sw $t0, 0($sp)
+subu $sp, $sp, 4
+sw $t1, 0($sp)
+subu $sp, $sp, 4
+sw $t2, 0($sp)
+subu $sp, $sp, 4
+sw $t3, 0($sp)
+subu $sp, $sp, 4
+sw $t4, 0($sp)
+subu $sp, $sp, 4
+sw $t5, 0($sp)
+subu $sp, $sp, 4
+sw $t6, 0($sp)
+subu $sp, $sp, 4
+sw $t7, 0($sp)
+subu $sp, $sp, 4
+sw $t8, 0($sp)
+subu $sp, $sp, 4
+sw $t9, 0($sp)
 sub $sp, $sp, 16
 
 # inline implementation of PrintString
@@ -21,6 +41,16 @@ j user_main_epilogue
 
 user_main_epilogue:
 move $sp, $fp
+lw $t0, -4($sp)
+lw $t1, -8($sp)
+lw $t2, -12($sp)
+lw $t3, -16($sp)
+lw $t4, -20($sp)
+lw $t5, -24($sp)
+lw $t6, -28($sp)
+lw $t7, -32($sp)
+lw $t8, -36($sp)
+lw $t9, -40($sp)
 lw $fp, 0($sp)
 lw $ra, 4($sp)
 addu $sp, $sp, 4
